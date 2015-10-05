c3c32e26218a6dd9692652e3ee7fe59b0ec2e483
dimitriadisg <dimitriadisg@outlook.com>
2015-10-05 13:52:01 +0300

    Update index.php
    - Worked a little more on the front-end part of the code. Specifically added labels for all input tags.
    - Added some more comments.

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
 
+	//Specify a fixed way to identify custom classes
 	function __autoload($which_class)
 	{
 		include_once 'inc/class.'.$which_class.'.inc.php';
 	}
 	
+	include 'inc/helpers.php';
+	
+	//Print the page's head and menu (same for all pages of this webplace)
 	$strct = new Structure("Ad-marketting","Welcome to our FREE investment calculator tool !");
 	echo $strct->get_head();
 	echo $strct->get_menu();
 	
 	//Body here
+	
+	/*
+	** We have only front-end development here, the code is pretty straight forward,
+	** just create the prefered elements to fill the form and print them.
+	** Note: the names used here are the same as the names on the redirect page to
+	** keep things simple.
+	*/
+	$forms = new Forms();
 	
 	print('<h1 class="subheader">Calculate the investment\'s return</h1>');
 	print('<br>');
@@ -17,28 +29,27 @@
 	print
 		('
 			<div class="options big">
-				Aimed income per month: <br>
-				Average product value: <br>
-				Conversion percentage: <br>
-				URL webshop: <br>
-				Search data: <br>
+				'.$forms->label("monthly_income","Aimed income per month:").' <br>
+				'.$forms->label("product_value","Average product value:").' <br>
+				'.$forms->label("conversion_percentage","Conversion percentage:").' <br>
+				'.$forms->label("shop_url","URL webshop:").' <br>
+				'.$forms->label("keywords","Search data:").' <br>
 			</div>
 		');
 	
-	$forms_builder = new Forms();
 	print
 		('
 			<div class="options big">
-				<form method="POST" action="./investment_calc.php">
+				<form method="POST" autocomplete="off" action="./investment_calc.php">
 		');
 	print
 		(
-			$forms_builder->euro_input("monthly_income","100000","100000").' marge'.$forms_builder->marge_selector("marge",0).'<br>'.
-			$forms_builder->euro_input("product_value","10000","10000").'<br>'.
-			$forms_builder->percentage_input("conversion_percentage","1","1").'<br>'.
-			$forms_builder->text_input("shop_url","webshop.nl").'<br>'.
-			$forms_builder->textarea("keywords","").'<br>'.
-			'<br>'.$forms_builder->submit("Proceed to calculator").'
+			$forms->euro_input("monthly_income",DEFAULT_INCOME,DEFAULT_INCOME).' '.$forms->label("marge","marge").$forms->marge_selector("marge",0).'<br>'.
+			$forms->euro_input("product_value",DEFAULT_PVALUE,DEFAULT_PVALUE).'<br>'.
+			$forms->percentage_input("conversion_percentage",DEFAULT_CPERCENTAGE,DEFAULT_CPERCENTAGE).'<br>'.
+			$forms->text_input("shop_url",DEFAULT_SURL,DEFAULT_SURL).'<br>'.
+			$forms->textarea("keywords",DEFAULT_KEYS).'<br>'.
+			'<br>'.$forms->submit("Proceed to calculator").'
 				</form>
 			</div>
 		');
@@ -56,5 +67,6 @@
 		');
 	//End body
 	
+	//Print the page's footer (same for all pages of this webplace)
 	echo $strct->get_footer();
 ?>
