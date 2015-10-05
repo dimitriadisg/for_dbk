9a385ad86fce350d1e26929f503b16795d24c4b9
dimitriadisg <dimitriadisg@outlook.com>
2015-10-05 13:42:34 +0300

    Create constants.php
    Gathered all php constants in one file.

---
+++ inc/constants.php
@@ -0,0 +1,23 @@
+<?php
+
+	//Define constants
+	define("DEFAULT_INCOME", 100000.0);
+	define("DEFAULT_PVALUE", 10000.0);
+	define("DEFAULT_CPERCENTAGE", 1.0);
+	define("DEFAULT_SURL", "webshop.nl");
+	define("DEFAULT_KEYS", "");
+	define("DEFAULT_MARGE", 0);
+	define("DEFAULT_DIRECT", 20);
+	define("DEFAULT_GOOGLE", 25);
+	define("DEFAULT_ADWORDS", 35);
+	define("DEFAULT_LINKS", 10);
+	define("DEFAULT_UNPAID", 10);
+	define("DEFAULT_ADDITIONAL", 0);
+	define("DEFAULT_MB_PERCENTAGE", 0);
+	define("DEFAULT_MB_EURO", 0);
+	define("MY_SKYPE","dimitriadis.h.george");
+	define("MY_MAIL","dimitriadisg@outlook.com");
+	define("GITHUB_HOME","https://github.com/");
+	define("MY_GITHUB","dimitriadisg");
+	
+?>
